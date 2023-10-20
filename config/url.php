c6a0ec14282bf566a6a29de7c1792d8320252ede
Marcos-Teste/QA-SEPLAN <marcossistemas.ac@gmail.com>
2023-10-20 10:13:25 -0500

    Projeto Agenda com CRUD completo

---
+++ config/url.php
@@ -0,0 +1,7 @@
+<?php 
+
+ /* o erro deste comentado era o https */
+/* $BASE_URL = "https://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . '?') . '/'; */
+    $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . '?') . '/';
+
+?>
